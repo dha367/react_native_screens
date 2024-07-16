9bcd522b1a46f289503d99cbc56c21797708580d
Dhanusha Thilakarathne <dhanushat@lbfinance.local>
2024-07-16 18:39:47 +0530

    fix:add new Profile

---
+++ Screens/Profile.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import CustomButton from '../Componats/CoustomeButton';
+import TextButton from '../Componats/TextButton';
+
+const Profile=()=>{
+    return(
+      <View>
+        <Text>hi profile</Text>
+      </View>
+    );
+}
+
+export default Profile;

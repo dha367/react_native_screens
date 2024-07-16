ff3c09cd2f07228cc858c75f481332a08469a28f
Dhanusha Thilakarathne <dhanushat@lbfinance.local>
2024-07-16 19:13:27 +0530

    fix:profile style

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -5,11 +5,19 @@ import TextButton from '../Componats/TextButton';
 
 const Profile=()=>{
     return(
-      <View>
+      <View style={styles.view}>
         <Text>hi profile</Text>
         {/* <Text>new profile i am dhanusha deshanu</Text> */}
+        <View><Text>new wallent balance</Text></View>
       </View>
     );
 }
+
+const styles=StyleSheet.create({
+  view:{
+    flex:1,
+    backgroundColor:'#ffff'
+  }
+})
 
 export default Profile;
